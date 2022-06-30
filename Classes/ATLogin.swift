9ca27179d9b89e49bb3e4f7e83eb616366be364c
mobiledev9 <jiyanp1592@gmail.com>
2022-06-30 10:43:56 +0530

    - ads file

---
+++ Classes/ATLogin.swift
@@ -0,0 +1,20 @@
+//
+//  ATLogin.swift
+//  APsAdsTracker
+//
+//  Created by dds on 30/06/22.
+//
+
+import Foundation
+
+public class ATLogin {
+    
+    public init () {
+        
+    }
+    
+    public func showAdsDetail () {
+        print("My User Name")
+    }
+    
+}
---
+++ Example/Pods/Target Support Files/APsAdsTracker/APsAdsTracker-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_APsAdsTracker : NSObject
+@end
+@implementation PodsDummy_APsAdsTracker
+@end
